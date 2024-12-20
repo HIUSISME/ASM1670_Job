d9a46ebd431677b92d722050d603255f1afa2d05
HIUSISME <ahihi3116@gmail.com>
2024-12-20 18:18:02 +0700

    Add project files.

---
+++ ASM1670_Job/Controllers/AccountController.cs
@@ -0,0 +1,139 @@
+﻿using ASM1670_Job.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASM1670_Job.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users.ToList();
+            var userRoles = new Dictionary<string, string>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                var roleNames = string.Join(", ", roles);
+                userRoles.Add(user.Id, roleNames);
+            }
+
+            ViewBag.UserRoles = userRoles;
+
+            // Lấy danh sách các vai trò và lưu vào ViewBag.Roles
+            var rolesList = _roleManager.Roles.Select(r => r.Name).ToList();
+            ViewBag.Roles = rolesList;
+
+            return View(users);
+        }
+        //Xóa người dùng
+        public IActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = _userManager.FindByIdAsync(id).Result;
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        //xác nhận xóa người dùng
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View(user);
+        }
+        //Khóa người dùng
+            public async Task<IActionResult> LockAccount(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                await _userManager.SetLockoutEndDateAsync(user, DateTimeOffset.MaxValue);
+            }
+            return RedirectToAction("Index");
+        }
+        // Mở khóa người dùng
+        public async Task<IActionResult> UnlockAccount(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                await _userManager.SetLockoutEndDateAsync(user, null);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+
+        // Update Role người dùng
+        public async Task<IActionResult> UpdateRole(string userId, string selectedRole)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var currentRoles = await _userManager.GetRolesAsync(user);
+
+            if (currentRoles.Contains(selectedRole))
+            {
+                // Role đã tồn tại cho người dùng, không cần thực hiện gì cả, chuyển hướng trở lại Index
+                return RedirectToAction("Index");
+            }
+
+            // Xóa role hiện tại của người dùng
+            await _userManager.RemoveFromRolesAsync(user, currentRoles);
+
+            // Cập nhập role mới cho người dùng
+            await _userManager.AddToRoleAsync(user, selectedRole);
+
+            // Chuyển hướng trở lại Index sau khi đã cập nhật vai trò
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ ASM1670_Job/Controllers/RolesController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+
+namespace ASM1670_Job.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //GET Create() displays a form for the user to enter information about the new role
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole model)
+        {
+            if (!await _roleManager.RoleExistsAsync(model.Name))
+            {
+                await _roleManager.CreateAsync(new IdentityRole(model.Name));
+            }
+            return RedirectToAction("Index");
+        }
+        //POST Create(IdentityRole model) handles creating the new role.
+        //If the role does not exist, it will be created and the user will then be redirected to the Index page.
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+        //GET Edit(string id) displays a form for users to edit information of the current role
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, IdentityRole model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var role = await _roleManager.FindByIdAsync(id);
+                    if (role != null)
+                    {
+                        role.Name = model.Name;
+                        var result = await _roleManager.UpdateAsync(role);
+                        if (result.Succeeded)
+                        {
+                            return RedirectToAction(nameof(Index));
+                        }
+                    }
+                    return View(model);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return NotFound();
+                }
+            }
+            return View(model);
+        }
+        //POST Edit(string id, IdentityRole model) handles updating the role in the database if the input is valid and the role exists
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+        //Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                var result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return NotFound();
+        }
+
+
+    }
+}
